c23fabcde0a08c717d355b08632913dc4ff1b669
Swagoto Surjo Dutta <swagotosurjodutta@gmail.com>
2025-02-08 03:16:13 +0530

    08022025

--- hydrostatic.m
+++ hydrostatic.m
@@ -3,6 +3,7 @@ ns = size(megaarray,3);
 sum_zy = zeros(2,1);
 sum_v = zeros(3,1);
 sum_a = zeros(2,1);
+flag = 0;
 sum_zy_prev = zeros(2,1);
     for i = 1:ns     %ns
         if t_i(i,1)>0
@@ -14,8 +15,10 @@ sum_zy_prev = zeros(2,1);
                     %     disp(sum_zy);
                     % end
             end
-            sum_zy(2) = sum_zy(2) + (0.5)^2*(megaarray(t_i(i,1),1,i)+z_i)*(-megaarray(t_i(i,1),1,i)+z_i)*(y_t+megaarray(t_i(i,1),2,i));%multiply z_i (for the linear line)
-            sum_zy(1) = sum_zy(1) + 0.5*(-megaarray(t_i(i,1),1,i)+z_i)*(y_t+megaarray(t_i(i,1),2,i));
+            if t_i(i,2)==0
+                sum_zy(2) = sum_zy(2) + (0.5)^2*(megaarray(t_i(i,1),1,i)+z_i)*(-megaarray(t_i(i,1),1,i)+z_i)*(y_t+megaarray(t_i(i,1),2,i));%multiply z_i (for the linear line)
+                sum_zy(1) = sum_zy(1) + 0.5*(-megaarray(t_i(i,1),1,i)+z_i)*(y_t+megaarray(t_i(i,1),2,i));
+            end
                     % if(sum_zy(2) == 0)
             % disp(t_i(i,1));
             % disp(y_t);
@@ -23,21 +26,24 @@ sum_zy_prev = zeros(2,1);
             % if(sum_zy(1)==0)
             %     break;
             % end
-            if i<=ns-1
+            if flag==1
                 if i>1
                     sum_v(3) = sum_v(3) + delx(i-1)*(sum_zy(1)+sum_zy_prev(1))*(0.5)^2*(x(i)+x(i-1));
                     sum_v(2) = sum_v(2) + delx(i-1)*(sum_zy(2)+sum_zy_prev(2))*0.5;
                     sum_v(1) = sum_v(1) + delx(i-1)*(sum_zy(1)+sum_zy_prev(1))*0.5;
             
-            disp(sum_zy(1));
-                
-                    sum_a(1) = sum_a(1) + 0.5*(y_t+y_t_prev)*delx(i-1);
-                    sum_a(2) = sum_a(2) + (0.5)^2*(x(i)+x(i+1))*(y_t+y_t_prev)*delx(i-1);
+            % disp(sum_zy(1));
+            % disp(i);
+                    if t_i(i,2)==0
+                        sum_a(1) = sum_a(1) + 0.5*(y_t+y_t_prev)*delx(i-1);
+                        sum_a(2) = sum_a(2) + (0.5)^2*(x(i)+x(i+1))*(y_t+y_t_prev)*delx(i-1);
+                    end
                 end
             end
             sum_zy_prev(1) = sum_zy(1);
             sum_zy_prev(2) = sum_zy(2);
             y_t_prev = y_t;
+            flag = 1;
             %disp(sum_zy)
             % if (sum_zy(2)==-inf)
             %     disp(i);
--- main.m
+++ main.m
@@ -4,18 +4,62 @@ clc;
 load('sections.mat'); %code restricted to 100 datapoints per section
 
 n = size(sections,1);
-
-megaarray = zeros(100,2,106);
+ns = sections(1,1); %no. of sections
+for z_i = 0.1:0.1:13.8
+megaarray = zeros(100,2,ns);
 i=2;
 j=0;
 k = 1;
-ns = sections(1,1); %no. of sections
 
-z_i = input("z_i = ");
-t_i = zeros(ns,2);
+
+%z_i = input("z_i = ");
+t_i = zeros(ns,2); 
 delx = zeros(ns-1,1);
 flag=0;
 xarray = zeros(ns,1);
+
+% while(i+j<=n)   
+%     array = zeros(100,2);
+%     j = sections(i,2);
+% 
+%     if(k~=ns)
+%     x1 = sections(i,1);
+%     x2 = sections(i+j+1,1);
+%     end
+%     if z_i>sections(i+j,1)
+%         t_i(k,2) = 1;
+%     else
+%         t_i(k,2) = 0;
+%     end
+%     xarray(k) = sections(i,1);
+%     % delx(k) = x2-x1;
+%     for t=1:j-1
+% 
+%         array(t,1)=sections(t+i,1);
+%         array(t,2)=sections(t+i,2);
+%         array(t+1,1)=sections(t+i+1,1);
+%         array(t+1,2)=sections(t+i+1,2);
+%         if ((array(t+1,1)>=z_i)&&(array(t,1)<=z_i))
+%             t_i(k,1) = t;
+%             flag = 1;
+%             break;
+%         elseif (((array(t+1,1)<z_i)&&(array(t,1)<z_i)))
+%             t_i(k,1) = t;                    
+%             flag = 1;
+%         end
+%     end
+%     if (flag == 0)
+%             t_i(k,1) = 0;
+%     end
+%     flag = 0;
+%     megaarray(:,:,k) = array;
+%     if(k<ns)  
+%         delx(k) = x2-x1;
+%     end
+%     k = k+1;
+% 
+%     i = i+j+1;
+% end
 
 while(i+j<=n)   
     array = zeros(100,2);
@@ -25,7 +69,8 @@ while(i+j<=n)
     x1 = sections(i,1);
     x2 = sections(i+j+1,1);
     end
-    if z_i>sections(i+j,1)
+    %disp(sections(i+j,2));
+    if z_i>sections(i+j,2)
         t_i(k,2) = 1;
     else
         t_i(k,2) = 0;
@@ -34,10 +79,10 @@ while(i+j<=n)
     % delx(k) = x2-x1;
     for t=1:j-1
 
-        array(t,1)=sections(t+i,1);
-        array(t,2)=sections(t+i,2);
-        array(t+1,1)=sections(t+i+1,1);
-        array(t+1,2)=sections(t+i+1,2);
+        array(t,1)=sections(t+i,2);
+        array(t,2)=sections(t+i,1);
+        array(t+1,1)=sections(t+i+1,2);
+        array(t+1,2)=sections(t+i+1,1);
         if ((array(t+1,1)>=z_i)&&(array(t,1)<=z_i))
             t_i(k,1) = t;
             flag = 1;
@@ -62,11 +107,12 @@ end
 
 [VCB,LCB,LCF,Disp] = hydrostatic(megaarray,delx,xarray,t_i,z_i);
 
-fprintf('Displacement = %d\n', Disp);
-fprintf('VCB = %d\n', VCB);
-fprintf('LCB = %d\n', LCB);
-fprintf('LCF = %d\n', LCF);
-
+% fprintf('Displacement = %d\n', Disp);
+% fprintf('VCB = %d\n', VCB);
+% fprintf('LCB = %d\n', LCB);
+% fprintf('LCF = %d\n', LCF);
+disp(Disp);
+end
 
 
 
